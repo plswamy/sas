762aee799cfdcc25b7e951c58821d51e7f7ba8f9
kvbollepalli <kvbollepalli@cirrus10.com>
2017-07-12 12:34:20 +0530

    modified sql script

--- src/main/resources/sql/db.sql
+++ src/main/resources/sql/db.sql
@@ -1,6 +1,36 @@
 CREATE DATABASE  IF NOT EXISTS `sas`;
 USE `sas`;
+
 --
+-- Table structure for table `users`
+--
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  `entity_status` varchar(32) NOT NULL,
+  `version` int(11) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password_hash` varchar(64) DEFAULT NULL,
+  `user_name` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_users_user_name` (`user_name`),
+  KEY `created_by` (`created_by`),
+  KEY `updated_by` (`updated_by`),
+  KEY `idx_users_email` (`email`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`),
+  CONSTRAINT `users_ibfk_2` FOREIGN KEY (`updated_by`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+INSERT INTO `users` (`id`, `created_at`, `created_by`, `updated_at`, `updated_by`, `entity_status`, `version`, `email`, `password_hash`, `user_name`)
+VALUES
+  (1, now(), 1, now(), NULL, 'ACTIVE', 1, 'test@test.com', 'Password1234', 'test'),
+  (2, now(), 1, now(), NULL, 'ACTIVE', 1, 'test@test.com', 'Password1234', 'admin');
+  
+  --
 -- Table structure for table `roles`
 --
 
@@ -29,34 +59,6 @@ VALUES
   (now(), 'ACTIVE', 1, 'ROLE_ADMIN');
 
 
---
--- Table structure for table `users`
---
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `created_at` datetime NOT NULL,
-  `created_by` int(11) DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  `updated_by` int(11) DEFAULT NULL,
-  `entity_status` varchar(32) NOT NULL,
-  `version` int(11) NOT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `password_hash` varchar(64) DEFAULT NULL,
-  `user_name` varchar(32) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `idx_users_user_name` (`user_name`),
-  KEY `created_by` (`created_by`),
-  KEY `updated_by` (`updated_by`),
-  KEY `idx_users_email` (`email`),
-  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`),
-  CONSTRAINT `users_ibfk_2` FOREIGN KEY (`updated_by`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
-
-INSERT INTO `users` ( `created_at`, `created_by`, `updated_at`, `updated_by`, `entity_status`, `version`, `email`, `password_hash`, `user_name`)
-VALUES
-  (now(), 1, now(), NULL, 'ACTIVE', 1, 'test@test.com', 'Password1234', 'test'),
-  (now(), 1, now(), NULL, 'ACTIVE', 1, 'test@test.com', 'Password1234', 'admin');
 
 --
 -- Table structure for table `user_role`
